13cb73b51dec5f6aacf02a37650743c8b6920abf
kbouzidi <kr.bouzidi@gmail.com>
2016-01-13 11:12:58 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var logger = require('log4js').getLogger('Server');
+var bodyParser = require('body-parser');
+var app = express();
+
+// config
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined')); // Active le middleware de logging
+
+app.use(express.static(__dirname + '/public')); // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+
+logger.info('server start');
+
+app.get('/', function(req, res){
+    res.redirect('/login');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.listen(1313);
+
+
+
