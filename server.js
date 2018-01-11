f0ed0b93e96b4111b6b0fb19edc84864dd8d6139
TEBBOUNE Yacine <ty500362@iutnice.unice.fr>
2018-01-11 10:50:03 +0100

    ajout register

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ var morgan = require('morgan'); // Charge le middleware de logging
 var logger = require('log4js').getLogger('Server');
 var bodyParser = require('body-parser');
 var app = express();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: 'ecommerce'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result);
+    });
+});
 
 // config
 app.set('view engine', 'ejs');
@@ -27,8 +43,25 @@ app.get('/register', function(req, res){
     res.render('register');
 });
 
+app.post("/registering", function(req, res) {
+    var objBD = con;
+
+    var post = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    objBD.query('INSERT INTO clients VALUES ?', post, function(error) {
+        if (error) {
+            console.log(error.message);
+        } else {
+            console.log('success');
+        }
+    });
+
 app.get('/ping', function(req, res){
     res.send('Salut tout le monde !');
 });
+
 
 app.listen(process.env.PORT||1313);
