fc8a4359ab8dfa77700626e6eb29ecc19476ad1b
Yacine TEBBOUNE <yacine.tebboune@etu.unice.fr>
2018-01-18 00:02:55 +0100

    test

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 var pg = require('pg');
 delete pg.native;
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    protocol: 'postgres',
-    database: 'dacpbufuo93isb',
-    username: 'kmupkwyciyrfjk',
-    password: '50798bea3445d6a28d8d3303b7996573de881118dc48a82f7945845c9ca69ee7',
-    host: 'ec2-54-217-243-160.eu-west-1.compute.amazonaws.com',
-    port : '5432'
-});
 // const sequelize = new Sequelize({
-//     dialect: 'mysql',
-//     database: 'localhost',
-//     username: 'root',
-//     password: 'root'
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     database: 'dacpbufuo93isb',
+//     username: 'kmupkwyciyrfjk',
+//     password: '50798bea3445d6a28d8d3303b7996573de881118dc48a82f7945845c9ca69ee7',
+//     host: 'ec2-54-217-243-160.eu-west-1.compute.amazonaws.com',
+//     port : '5432'
 // });
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    database: 'localhost',
+    username: 'root',
+    password: 'root'
+});
 
 module.exports = sequelize;
