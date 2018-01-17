09a1babdc8830e61bdcbd4c76649683cf357c4c5
Yacine TEBBOUNE <yacine.tebboune@etu.unice.fr>
2018-01-17 22:32:42 +0100

    Postgre

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'postgres',
+    protocol: 'postgres',
     database: 'dacpbufuo93isb',
     username: 'kmupkwyciyrfjk',
     password: '50798bea3445d6a28d8d3303b7996573de881118dc48a82f7945845c9ca69ee7',
-    host: 'localhost'
+    host: 'ec2-54-217-243-160.eu-west-1.compute.amazonaws.com',
+    port : 5432
 });
 
 module.exports = sequelize;
