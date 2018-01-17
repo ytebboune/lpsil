79708d42b476812909af2cc7f33906b45a716c3b
TEBBOUNE Yacine <ty500362@iutnice.unice.fr>
2018-01-17 17:45:02 +0100

    annulation

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-    dialect: 'postgre',
+    dialect: 'mysql',
     database: 'ecommerce',
     username: 'root',
     password: 'root',
