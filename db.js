609f62ec56306696e7220bbf0811bd572d279832
Yacine TEBBOUNE <yacine.tebboune@etu.unice.fr>
2018-01-17 23:50:29 +0100

    test

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize({
     username: 'kmupkwyciyrfjk',
     password: '50798bea3445d6a28d8d3303b7996573de881118dc48a82f7945845c9ca69ee7',
     host: 'ec2-54-217-243-160.eu-west-1.compute.amazonaws.com',
-    port : 5432
+    port : '5432'
 });
 // const sequelize = new Sequelize({
 //     dialect: 'mysql',
